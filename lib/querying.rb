35c15bc6c0b586bcd5a8352537891cc0d85dcbb6
Kiri Um <kirirotha@gmail.com>
2020-07-23 18:17:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM characters JOIN series ON characters.author_id = series.author_id"
+  "SELECT series.title FROM characters JOIN series ON characters.author_id = series.author_id WHERE COUNT(series.title) = MAX"
 end
 
 def select_character_names_and_number_of_books_they_are_in
